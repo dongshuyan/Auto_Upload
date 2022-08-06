679c616f45a6a3860f2d7c6fbdaa7ed80335a898
dongshuyan <ssauterne@qq.com>
2022-08-06 20:54:42 +0800

    au.yaml.example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Auto_Upload
-Upload torrent to PT sites  Automatically.
+# Auto_Upload  
+自动将本地资源发布到PT站  
+Upload local resources to PT trackers automatically.  
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import io
+
+setup(
+    name = "auto_upload",     
+    version = "0.0.5", 
+    keywords = ["pip", "auto_upload","auto","upload","PT","private tracker"],            
+    description = "Upload local resources to PT trackers automatically.",    
+    long_description=io.open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    license = "MIT Licence",    
+
+    entry_points = {
+        'console_scripts': ['auto_upload=auto_upload.main:main'],
+    },
+
+    url = "https://github.com/dongshuyan/Auto_Upload", 
+    author = "sauterne",            
+    author_email = "ssauterne@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    exclude_package_data = {'': ['__pycache__']},
+
+    platforms = "any",
+    install_requires = ["loguru","pathlib","typing","requests","bs4","lxml","datetime","selenium","qbittorrent-api","undetected_chromedriver","function_controler"]
+)
